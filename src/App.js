9f5940c8ae49b05690cf8907ae67e732b174df8b
sankalp chari <sankalpchari@gmail.com>
2020-05-27 10:51:32 +0530

    header&menu added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Homepage from './pages/homepage/Homepage.componant';
 import {Route,Switch} from 'react-router-dom';
 import Shop from './pages/shop/Shop.component.jsx';
+import Header from './components/header/Header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
           <Route exact path='/' component={Homepage} />
           <Route exact path='/Shop' component={Shop} />
